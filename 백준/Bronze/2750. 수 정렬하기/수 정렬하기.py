a6fd38808079a65fdd82ab0e99896c1a469ebf2f
jangse0 <151913785+jangse0@users.noreply.github.com>
2024-05-01 02:12:34 +0900

    Update 수 정렬하기.py

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -4,6 +4,11 @@ l=[]
 for i in range(n):
     l.append(int(input()))
 
-l.sort()
-for i in range(n):
+for i in range(n-1):
+    idx_min=i
+    for j in range(i+1, n):
+        if l[idx_min]>l[j]:
+            idx_min=j
+    l[i], l[idx_min]=l[idx_min], l[i]
     print(l[i])
+print(l[-1])
