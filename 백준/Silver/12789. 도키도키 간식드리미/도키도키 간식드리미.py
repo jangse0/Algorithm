fa08c78cbfcc60210824c96c7aeee040de569797
jangse0 <151913785+jangse0@users.noreply.github.com>
2024-05-01 17:45:45 +0900

    Update 도키도키 간식드리미.py

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -21,3 +21,27 @@ if len(stack)==0:
     print("Nice")
 else:
     print("Sad")
+'''
+n=int(input())
+l=list(map(int, input().split()))
+stack=[]
+order=1
+
+while order<=n:
+    if (len(l)!=0) and l[0]==order:
+        l.pop(0)
+        order+=1
+        continue
+    elif len(stack)!=0 and stack[-1]==order:
+        stack.pop()
+        order+=1
+        continue
+    else:
+        stack.append(l[0])
+        l.pop(0)
+
+if len(stack)==0 and len(l)==0:
+    print("Nice")
+else:
+    print("Sad")
+'''
