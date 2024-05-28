a881bbf55e2eaaf8ba44351c25a56d7d98b2636a
jangse0 <151913785+jangse0@users.noreply.github.com>
2024-05-29 05:23:20 +0900

    [Silver IV] Title: 덱 2, Time: 1528 ms, Memory: 70648 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+
+dq = deque()
+for i in range(n):
+    num = list(map(int, sys.stdin.readline().strip().split()))
+    l = len(dq)
+    if num[0] == 1:
+        dq.appendleft(num[1])
+    elif num[0] == 2:
+        dq.append(num[1])
+    elif num[0] == 3:
+        print(dq.popleft() if l else -1)
+    elif num[0] == 4:
+        print(dq.pop() if l else -1)
+    elif num[0] == 5:
+        print(len(dq))
+    elif num[0] == 6:
+        print(0 if l else 1)
+    elif num[0] == 7:
+        print(dq[0] if l else -1)
+    elif num[0] == 8:
+        print(dq[-1] if l else -1)
